fec40bbe7159854dbcb6c6f9ab81e28aa624e5d8
Andreas Herrmann <andreas.herrmann@tweag.io>
2020-04-03 16:28:29 +0200

    Trace node events

--- index.test.js
+++ index.test.js
@@ -1,4 +1,8 @@
+// Loaded from common-js format
+// node_modules/jest-websocket-mock/lib/jest-websocket-mock.cjs.js
 const WS = require("jest-websocket-mock").default;
+// Loaded from js format
+// node_modules/mock-socket/dist/mock-socket.js"
 const Server = require("mock-socket").Server;
 
 test("jest-websocket-mock", () => {
